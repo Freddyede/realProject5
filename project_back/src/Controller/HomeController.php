03f3cec82de2d7cb077de19c454be6f3e01f997e
Franck <patouillardfranck3@gmail.com>
2023-01-26 21:29:01 +0100

    add first back road

--- project_back/src/Controller/HomeController.php
+++ project_back/src/Controller/HomeController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): JsonResponse
-    {
+    public function index(): JsonResponse {
+        $request = new Response();
+        $datas = [
+            'users' => [
+                ['id'=>1,'lastName'=>'Patouillard', 'firstName'=>'Franck'],
+                ['id'=>2,'lastName'=>'Patouillard', 'firstName'=>'Hugo']
+            ]
+        ];
         return $this->json([
-            'message' => 'Welcome to my new controller!',
-            'path' => 'src/Controller/HomeController.php',
+            'status code' => $request->getStatusCode(),
+            'datas' => $datas
         ]);
     }
 }
